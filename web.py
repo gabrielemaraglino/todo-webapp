5e5b0951d1a6a0eb4241d51e4fa7f3625d6ae02f
gabrielemaraglino <gabrielemaraglino.04@gmail.com>
2024-08-30 22:12:44 +0200

    Handle entering an already existing todo

--- web.py
+++ web.py
@@ -22,11 +22,11 @@ st.title("Minimalistic Todo-app")
 st.markdown("*Increase your productivity.*")
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+    checkbox = st.checkbox(todo, key=todo + str(index))
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
-        del session_state[todo]
+        del session_state[todo + str(index)]
         st.rerun()
 
 st.text_input(label="", placeholder="Add new todo...",
